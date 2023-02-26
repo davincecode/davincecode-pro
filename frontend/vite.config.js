8956436f276e75dd4227074f2823800cbd80b9a1
davincecode <vncntybnz@gmail.com>
2023-02-26 16:11:36 -0700

    Update build to public

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import sassDts from 'vite-plugin-sass-dts';
-import path from 'path';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import sassDts from "vite-plugin-sass-dts"
+import path from "path"
 
 export default defineConfig({
   plugins: [react(), sassDts()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@container': path.resolve(__dirname, './src/container'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@container": path.resolve(__dirname, "./src/container"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "public",
   },
-});
-
+})
